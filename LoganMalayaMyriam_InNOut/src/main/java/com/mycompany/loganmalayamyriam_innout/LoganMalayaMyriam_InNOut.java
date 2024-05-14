75c87c5f879fcb32ecf1a1e9c0e69612e69c593c
Yeast404 <66443659+Yeast404@users.noreply.github.com>
2024-05-14 10:10:58 -0700

    Add files via upload

---
+++ LoganMalayaMyriam_InNOut/src/main/java/com/mycompany/loganmalayamyriam_innout/LoganMalayaMyriam_InNOut.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loganmalayamyriam_innout;
+
+/**
+ *
+ * @author hooda
+ */
+public class LoganMalayaMyriam_InNOut {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
